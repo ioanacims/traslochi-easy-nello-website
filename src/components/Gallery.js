d510d98dccb6f9edf4e28999836242f8875dd2b1
Laura Cimpoesu <laura.cimpoesu@mangrovia.io>
2024-04-12 17:15:59 +0200

    better height

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -9,17 +9,17 @@ export const Gallery = () => {
       <div className="border border-white p-28 rounded-lg">
         <div className="flex justify-around content-center flex-wrap ">
           <img
-            className="w-96 h-80 mt-6"
+            className="w-96 h-64 mt-6"
             src={require("../media/gallery/3.jpg")}
             alt="Lavoro di costruzione"
           />
           <img
-            className="w-96 h-80 mt-6"
+            className="w-96 h-64 mt-6"
             src={require("../media/gallery/8.jpg")}
             alt="Progetto completato"
           />
           <img
-            className="w-96 h-80 mt-6"
+            className="w-96 h-64 mt-6"
             src={require("../media/gallery/25.jpg")}
             alt="Dettaglio di costruzione"
           />
@@ -27,34 +27,34 @@ export const Gallery = () => {
         {/* second */}
         <div className="flex justify-around content-center flex-wrap">
           <img
-            className="w-96 h-80 mt-6"
+            className="w-96 h-64 mt-6"
             src={require("../media/gallery/26.jpg")}
             alt="Transizione del lavoro"
           />
           <img
-            className="w-96 h-80 mt-6"
+            className="w-96 h-64 mt-6"
             src={require("../media/gallery/12.jpg")}
             alt="Cucina completata"
           />
           <img
-            className="w-96 h-80 mt-6"
+            className="w-96 h-64 mt-6"
             src={require("../media/gallery/15.jpg")}
             alt="Dettaglio di finitura"
           />
         </div>
         <div className="flex justify-around content-center flex-wrap">
           <img
-            className="w-96 h-80 mt-6"
+            className="w-96 h-64 mt-6"
             src={require("../media/gallery/17.jpg")}
             alt="Progetto completato"
           />
           <img
-            className="w-96 h-80 mt-6"
+            className="w-96 h-64 mt-6"
             src={require("../media/gallery/5.jpg")}
             alt="Team di lavoro"
           />
           <img
-            className="w-96 h-80 mt-6"
+            className="w-96 h-64 mt-6"
             src={require("../media/gallery/10.jpg")}
             alt="Realizzazione di un lavoro"
           />
